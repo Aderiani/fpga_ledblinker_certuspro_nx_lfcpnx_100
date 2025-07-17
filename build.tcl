c0cd95b7f07cae766d9aaecce3f21320140de3ab
Adrian <adrian.aderiani@exobiosphere.com>
2025-07-17 10:04:15 +0200

    gitignore added

--- build.tcl
+++ build.tcl
@@ -9,31 +9,31 @@ prj_add_source "led_blinker.v"
 prj_add_source "led_blinker_top.v"
 
 # Add constraints files
-prj_add_source "led_blinker.pdc" -work "impl_1"
-prj_add_source "led_blinker.sdc" -work "impl_1"
+prj_add_source "led_blinker.pdc"
+prj_add_source "led_blinker.sdc"
 
 # Set top level module
-prj_set_impl_opt -impl "impl_1" top "led_blinker_top"
+prj_set_impl_opt top "led_blinker_top"
 
 # Synthesis options
-prj_set_impl_opt -impl "impl_1" syn_keep_hierarchy "rebuilt"
-prj_set_impl_opt -impl "impl_1" syn_freq "100.0"
+prj_set_impl_opt {include path} ""
+prj_set_impl_opt {syn_keep_hierarchy} "rebuilt"
+prj_set_impl_opt {syn_frequency} "100.0"
 
-# Implementation options
-prj_set_impl_opt -impl "impl_1" map_auto_gsr "true"
-prj_set_impl_opt -impl "impl_1" map_prioritize_pwrrail "false"
+# Implementation options  
+prj_set_impl_opt {buskeep} "0"
 
 # Save project
 prj_save
 
 # Run synthesis
-prj_run_synthesis -impl "impl_1" -task "Synplify_Synthesis"
+prj_run_synthesis
 
 # Run place and route
-prj_run_map -impl "impl_1" -task "Map"
-prj_run_par -impl "impl_1" -task "Place_Route"
+prj_run_map
+prj_run_par
 
 # Generate bitstream
-prj_run_export -impl "impl_1" -task "Bitgen"
+prj_run_export
 
-puts "Build complete! Bitstream generated at: impl_1/led_blinker_impl_1.bit"
+puts "Build complete! Bitstream generated in impl_1 directory"
